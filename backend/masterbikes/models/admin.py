d44fb0a33acc9ab20ac4d97c6b3c287aeca09d25
ctomrp <cristian.rico.palma@gmail.com>
2023-05-31 17:36:48 -0400

    feat: modelo ORM

---
+++ backend/masterbikes/models/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(TipoReparacion)
+admin.site.register(TipoBicicleta)
+admin.site.register(FormaPago)
+admin.site.register(Cliente)
+admin.site.register(Reparacion)
+admin.site.register(Arriendo)
+admin.site.register(Bicicleta)
+admin.site.register(ArriendoBicicleta)
---
+++ backend/masterbikes/models/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+
+# Create your models here.
+class TipoReparacion(models.Model):
+    name = models.CharField(max_length=45, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.name)
+    
+class TipoBicicleta(models.Model):
+    name = models.CharField(max_length=45, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.name)
+
+class FormaPago(models.Model):
+    name = models.CharField(max_length=45, blank=False, null=False)
+
+    def __str__(self):
+        return str(self.name)
+
+class Cliente(models.Model):
+    user_name = models.CharField(max_length=45, blank=False, null=False)
+    email = models.EmailField(max_length=45, blank=False, null=False)
+    password = models.CharField(max_length=45, blank=False, null=False)
+ 
+    def __str__(self):
+        return str(self.user_name)
+    
+class Reparacion(models.Model):
+    fecha_reparacion = models.DateTimeField(max_length=45, blank=False, null=False)
+    reparable = models.BooleanField(null=True)
+    descripcion = models.TextField()
+    cliente = models.ForeignKey('Cliente',on_delete=models.CASCADE)
+    tipo_reparacion = models.ForeignKey('TipoReparacion',on_delete=models.CASCADE)
+
+    def __str__(self):
+        msn = f"{self.cliente} {self.tipo_reparacion}"
+        return str(msn)
+    
+class Arriendo(models.Model):
+    fecha_inicio = models.DateTimeField(max_length=45, null=False)
+    fecha_termino = models.DateTimeField(max_length=45, null=True)
+    deposito_garantia = models.BooleanField(null=True)
+    cliente = models.ForeignKey('Cliente',on_delete=models.CASCADE)
+    forma_pago = models.ForeignKey('FormaPago',on_delete=models.CASCADE)
+
+    def __str__(self):
+        msn = f"{self.cliente} {self.forma_pago}"
+        return str(msn)
+    
+class Bicicleta(models.Model):
+    color = models.CharField(max_length=45, blank=False, null=False)
+    tamano_aro = models.IntegerField(null=False)
+    tipo_bicicleta = models.ForeignKey('TipoBicicleta',on_delete=models.CASCADE)
+
+    def __str__(self):
+        msn = f"{self.tamano_aro} {self.tipo_bicicleta}"
+        return str(msn)
+    
+class ArriendoBicicleta(models.Model):
+    arriendo = models.ForeignKey('Arriendo',on_delete=models.CASCADE)
+    bicicleta = models.ForeignKey('Bicicleta',on_delete=models.CASCADE)
+
+    def __str__(self):
+        msn = f"{self.arriendo} {self.bicicleta}"
+        return str(msn)
