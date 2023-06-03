cef3f65e9da26f6847390ab93e2d08530969731d
RoberNar <rober@narbona.cl>
2023-06-03 10:26:06 -0400

    Registro completado.
    Cambiar redireccion a user y auto login.

--- backend/masterbikes/login/urls.py
+++ backend/masterbikes/login/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import login, register, guardarregistro,index
+from .views import auth_login, auth_register,index, formularioArriendo, exit
 
 urlpatterns = [
     path('', index,name='index'),
-    path('login',login,name='login'),
-    path('register', register,name='register'),
-    path('guardarregistro', guardarregistro,name='guardarregistro'),
+    path('auth_login',auth_login,name='auth_login'),
+    path('auth_register', auth_register,name='auth_register'),
+    path('formularioarriendo',formularioArriendo,name='formularioArriendo'),
+    path('logout',exit,name='exit'),
 
 ]
--- backend/masterbikes/login/views.py
+++ backend/masterbikes/login/views.py
@@ -1,28 +1,47 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from models.models import Cliente
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import RegistrationForm
+from django.contrib.auth.models import User
 
 
 # Create your views here.
-def login(request):
-    return(render(request,'login.html'))
-def register(request):
+def auth_login(request):
+    if  request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message= 'Nombre de usuario o contraseña incorrecto'
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return(render(request,'login.html'))
+def auth_register(request):
+    if request.method=='POST':
+        form = RegistrationForm(request.POST)
+        print(form)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+            user = User.objects.create_user(username=username, email=email, password=password)
+            login(request, user)
+            return redirect('auth_login')
+    else:
+        form = RegistrationForm()
     return(render(request,'register.html'))
 def index(request):
     return(render(request,'index.html'))
-def guardarregistro(request):
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        objCliente = Cliente.objects.create(
-            user_name = username,
-            email = email,
-            password = password
-        )
-        objCliente.save()
-        return(render(request,'login.html'))
-            #context = {
-           #     messages.error(request, 'Error, no se ingreso validamente')
-           # }
-           # return (render(request,context={'messages': messages.get_messages(request)})), redirect('register')
+
+@login_required
+def formularioArriendo(request):
+    return(render(request,'formularioArriendo.html'))
+
+def exit(request):
+    logout(request)
+    return redirect('auth_login')
