c6afd5412f6c307d6a389d7870ed6f581b756150
Bastian Urrutia <bastian.urrutiat@gmail.com>
2023-06-15 18:59:22 -0400

    algunas validaciones

---
+++ backend/masterbikes/login/static/js/v2.js
@@ -0,0 +1,62 @@
+
+var cant = 0;
+$(document).ready(function(){
+    $('#enviarID').attr('disabled', true);
+
+    // alert("automatico!!!");
+    // cant = $("#bicicletasID").length;
+    // alert(cant);
+    
+});
+
+$("#agregarID").click(function(){
+    // cant = $("#bicicletasID").length;
+    var cant = document.getElementById("carritoID").length;
+
+    // alert(cant);
+
+    if (cant > 0){
+        $('#enviarID').attr('disabled', false);
+        
+        $("#temporalID").remove();
+    }else{
+
+    }
+
+
+});
+
+$("#eliminarID").click(function(){
+    // cant = $("#bicicletasID").length;
+    var cant = document.getElementById("carritoID").length;
+
+    // alert(cant);
+
+    if (cant > 0){
+        $('#enviarID').attr('disabled', false);
+    }else{
+        $('#enviarID').attr('disabled', true);
+
+        $("#mensajeID").append("<div id='temporalID'>" + "Error. Debe arrendar al menos una bicicleta."
+        + "</div>");
+    }
+});
+
+$("#fechaInicioID").change(function(){
+    alert(Date.now());
+});
+
+
+/**
+ * 
+ * 
+ * 
+ * 
+
+
+$("#agregarID").click(function(){
+    alert("sigue funcionando");
+});
+
+
+ */
